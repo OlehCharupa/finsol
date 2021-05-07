70845c9001147bcd48b049fea5cfaad09333106c
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2021-05-07 14:00:01 +0300

    js

--- js/form.js
+++ js/form.js
@@ -2,10 +2,9 @@
     document.querySelector(".js-speaker-form").addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let client = new FormData(e.currentTarget).forEach((value, name) =>
+        new FormData(e.currentTarget).forEach((value, name) =>
             console.log(`${name}: ${value}`)
         );
-        console.log(client);
         e.currentTarget.reset();
     });
 })();
