2f15f4f177879cea85995f7a4caba18515705c71
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2021-05-08 19:08:22 +0300

    added link phone

--- js/form1.js
+++ js/form1.js
@@ -4,37 +4,44 @@
     document.querySelector("#js-form").addEventListener("submit", (e) => {
         e.preventDefault();
 
-        // const nameUser = document.querySelector("#name")
-        // const phone = document.querySelector("#phone")
-        // const email = document.querySelector("#email")
-        // const text = document.querySelector("#text")
-
-        // console.log(nameUser.value);
-        // console.log(nameUser.name);
-
-        const data = new FormData().forEach((name, value) => {
-            data.append(name, value);
-
-        })
-        // formData.append(phone.name, phone.value);
-        // formData.append(email.name, email.value);
-        // formData.append(text.name, text.value);
 
 
-        // console.log(data);
+
 
         const postData = async (url) => {
             try {
+
+                let data = new FormData();
+
+                let nm = document.getElementById("name");
+                let eml = document.getElementById("email");
+                let phn = document.getElementById("phone");
+                let txt = document.getElementById("text");
+                let result = document.getElementById("result")
+
+                data.append("name", nm.value);
+                data.append("email", eml.value);
+                data.append("phone", phn.value);
+                data.append("text", txt.value);
+
+
                 const response = await fetch(url, {
                     method: "POST",
-                    body: new FormData(data),
+                    body: data,
                 })
 
                 const json = await response.json()
                 console.log("Успех:", JSON.stringify(json));
-
+                result.classList.remove("invisible")
+                result.classList.remove("resultError")
+                result.classList.add("resultOk")
+                result.textContent = "Заявка успешно отпралена."
             } catch (error) {
                 console.error("Ошибка", error)
+                result.classList.remove("invisible")
+                result.classList.remove("resultOk")
+                result.classList.add("resultError")
+                result.textContent = "Ошибка. Попробуйте ещё раз."
             }
         }
 
